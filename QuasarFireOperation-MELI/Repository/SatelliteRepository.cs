3b2cf486f72ad6e38652d554efbb934445ab5d9b
emaafe <100393070+emaafe@users.noreply.github.com>
2022-03-14 14:54:34 -0300

    MELI - Upload 2
    Por Faltante de archivos

---
+++ QuasarFireOperation-MELI/Repository/SatelliteRepository.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using QuasarFireOperation_MELI.Utils;
+using QuasarFireOperation_MELI.Models;
+using System.Data.SqlClient;
+
+namespace QuasarFireOperation_MELI.Repository
+{
+    public class SatelliteRepository
+    {
+        /// <summary>
+        /// Consulta y Genera todos los satélites existenes en la Base de Datos.
+        /// </summary>
+        /// <returns>Lista que contiene los Satelites.</returns>
+        public List<Satellite> getAllSatellites()
+        {
+            List<Satellite> SatelliteList = new List<Satellite>();
+
+            Database DB = new Database();
+            string SQL = "SELECT ID_Satellite FROM Satellite";
+
+            DataTable Tabla = DB.Read(SQL);
+
+            if (Tabla != null && Tabla.Rows.Count > 0)
+            {
+                foreach (DataRow DR in Tabla.Rows)
+                {
+                    int IDSat = Convert.ToInt32(DR["ID_Satellite"].ToString());
+                    Satellite sat = getSatellite(IDSat);
+                    if (sat != null)
+                        SatelliteList.Add(sat);
+                }
+            }
+            else
+                SatelliteList = null;
+
+            return SatelliteList;
+        }
+        /// <summary>
+        /// Consulta y genera el satélite existente en la Base de Datos con el ID indicado.
+        /// </summary>
+        /// <param name="ID">ID del Satélite</param>
+        /// <returns>Objeto Satellite que contiene los datos del mismo.</returns>
+        public Satellite getSatellite(int ID)
+        {
+            if (ID <= 0)
+                return null;
+
+            Satellite sat = new Satellite();
+            Database DB = new Database();
+            string SQL = "SELECT s.ID_Satellite, s.Name_Satellite, sl.X_Coord, sl.Y_Coord, sh.Distance, sh.Message FROM Satellite s";
+            SQL = SQL + " INNER JOIN SatelliteLocation sl ON sl.ID_Satellite = s.ID_Satellite AND sl.Until_Location IS NULL";
+            SQL = SQL + " LEFT JOIN SatelliteHistory sh ON sh.ID_Satellite = s.ID_Satellite AND sh.Until_History IS NULL";
+            SQL = SQL + " WHERE s.ID_Satellite = " + ID.ToString();
+            DataTable Tabla = DB.Read(SQL);
+
+            if (Tabla != null && Tabla.Rows.Count == 1)
+            {
+                var DR = Tabla.Rows[0];
+                sat.ID = Convert.ToInt32(DR["ID_Satellite"].ToString());
+                sat.Name = DR["Name_Satellite"].ToString();
+                sat.Location = new Coordinate() { X = float.Parse(DR["X_Coord"].ToString()), Y = float.Parse(DR["Y_Coord"].ToString()) };
+                
+                if (!DR.IsNull("Distance"))
+                    sat.Distance = float.Parse(DR["Distance"].ToString());
+                else
+                    sat.Distance = null;
+
+                if (!DR.IsNull("Message"))
+                    sat.Message = DR["Message"].ToString().Split(";");
+                else
+                    sat.Message = null;
+            }
+            else
+                sat = null;
+
+            return sat;
+        }
+        /// <summary>
+        /// Consulta y genera el satélite existente en la Base de Datos con el nombre indicado.
+        /// </summary>
+        /// <param name="satName">Nombre del Satélite</param>
+        /// <returns>Objeto Satellite que contiene los datos del mismo.</returns>
+        public Satellite getSatellite(string satName)
+        {
+            if (satName == null || satName == "")
+                return null;
+
+            Database DB = new Database();
+            string SQL = "SELECT ID_Satellite FROM Satellite WHERE Name_Satellite LIKE '" + satName.ToUpper() + "'";
+
+            int satID = 0;
+            DataTable Tabla = DB.Read(SQL);
+            if (Tabla != null && Tabla.Rows.Count == 1)
+                satID = Convert.ToInt32(Tabla.Rows[0]["ID_Satellite"]);
+
+            return getSatellite(satID);
+        }
+        /// <summary>
+        /// Actualización de los Datos de un Satélite.
+        /// </summary>
+        /// <param name="sat">Objeto Satellite a modificar.</param>
+        /// <returns>True en caso de actualización correcta y False si falla.</returns>
+        public bool updateSatellite(Satellite sat)
+        {
+            bool Flag = false;
+
+            if (verificateSatellite(sat))
+            {
+                Database DB = new Database();
+                DateTime ActDate = DateTime.Now;
+
+                string SQL_Update = "UPDATE SatelliteHistory SET Until_History = '" + ActDate.ToString("yyyy-MM-dd HH:mm:ss") + "'";
+                SQL_Update = SQL_Update + " WHERE Until_History IS NULL AND ID_Satellite = " + sat.ID.ToString();
+
+                string SQL_Insert = "INSERT INTO SatelliteHistory (ID_Satellite, Distance, Message, From_History) VALUES (";
+                SQL_Insert = SQL_Insert + sat.ID.ToString() + "," + sat.Distance.ToString().Replace(",",".") + ", '" + String.Join(";", sat.Message) + "', '";
+                SQL_Insert = SQL_Insert + ActDate.ToString("yyyy-MM-dd HH:mm:ss") + "')";
+
+                SqlConnection conn = DB.getSQLConnection();
+                conn.Open();
+                SqlTransaction Tran = null;
+
+                try
+                {
+                    Tran = conn.BeginTransaction();
+                    DB.Query(SQL_Update, conn, Tran);
+                    DB.Query(SQL_Insert, conn, Tran);
+                    Tran.Commit();
+                    Flag = true;
+                }
+                catch(Exception ex)
+                {
+                    if(Tran != null)
+                        Tran.Rollback();
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+            return Flag;
+        }
+        /// <summary>
+        /// Controla que un objeto Satellite posea todos los datos necesarios.
+        /// </summary>
+        /// <param name="sat">Objeto Satellite a controlar.</param>
+        /// <returns>True si posee todos los datos correctos. False en caso contrario.</returns>
+        private bool verificateSatellite(Satellite sat)
+        {
+            bool Flag = true;
+
+            if (Flag) Flag = sat.ID > 0 ? true : false;
+            if (Flag) Flag = sat.Distance > 0 ? true : false;
+            if (Flag) Flag = sat.Location != null ? true : false;
+            if (Flag) Flag = sat.Message != null && sat.Message.Length > 0 ? true : false;
+            if (Flag) Flag = sat.Name != "" ? true : false;
+
+            return Flag;
+        }
+    }
+}
---
+++ QuasarFireOperation-MELI/Services/SatelliteService.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QuasarFireOperation_MELI.Models;
+using QuasarFireOperation_MELI.Utils;
+using QuasarFireOperation_MELI.Repository;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace QuasarFireOperation_MELI.Services
+{
+    public class SatelliteService
+    {
+        #region Public Functions
+        /// <summary>
+        /// Genera la respuesta para el controlador TopSecret
+        /// </summary>
+        /// <param name="JSON">JSON con datos del body del Controlador.</param>
+        /// <returns>Respuesta del Controlador.</returns>
+        public dynamic getTopSecretResponse(dynamic JSON)
+        {
+            SatelliteRepository repo = new SatelliteRepository();
+            List<Satellite> SatelliteList = deserializeTopSecret(JSON);
+
+            if (SatelliteList == null || SatelliteList.Count != 3)
+                return null;
+
+            if (!validateSatelliteList(SatelliteList))
+                return null;
+
+            foreach (Satellite s in SatelliteList)
+            {
+                Satellite sat = repo.getSatellite(s.Name.ToUpper());
+                sat.Distance = s.Distance;
+                sat.Message = s.Message;
+                repo.updateSatellite(sat);
+            }
+
+            SatelliteList.Clear();
+            SatelliteList = repo.getAllSatellites();
+
+            Coordinate coordResponse = getLocation(SatelliteList[0], SatelliteList[1], SatelliteList[2]);
+            string messageResponse = getMessage(SatelliteList[0].Message, SatelliteList[1].Message, SatelliteList[2].Message);
+
+            if (coordResponse != null && messageResponse != null)
+            {
+                TopSecretResponse response = new TopSecretResponse();
+                response.message = messageResponse;
+                response.position = coordResponse;
+                dynamic jsonResponse = JsonConvert.SerializeObject(response);
+                return jsonResponse;
+            }
+            else
+                return null;
+        }
+        /// <summary>
+        /// Genera la respuesta para el controlador TopSecretSplit [GET]
+        /// </summary>
+        /// <param name="satellite_name">Nombre del satélite pasado como parámetro en el Controlador.</param>
+        /// <returns>Respuesta del Controlador.</returns>
+        public dynamic getTopSecretSplitResponse(string satellite_name)
+        {
+            SatelliteRepository repo = new SatelliteRepository();
+
+            Satellite sat = repo.getSatellite(satellite_name.ToUpper());
+
+            if (sat != null)
+            {
+                TopSecretResponse response = new TopSecretResponse();
+                response.message = sat.Message == null || sat.Message.Length <= 0 ? "No hay suficiente información para revelar el mensaje" : String.Join(" ", sat.Message);
+                response.position = sat.Location;
+                dynamic jsonResponse = JsonConvert.SerializeObject(response);
+                return jsonResponse;
+            }
+            else
+                return null;
+        }
+        /// <summary>
+        /// Genera la respuesta para el controlador TopSecretSplit [POST]
+        /// </summary>
+        /// <param name="satellite_name">Nombre del satélite pasado en el endpoint.</param>
+        /// <param name="JSON">JSON con datos del body del Controlador.</param>
+        /// <returns>True en caso de actualizar los datos de forma correcta. False en caso contrario.</returns>
+        public bool getTopSecretSplitResponse(string satellite_name, dynamic JSON)
+        {
+            SatelliteRepository repo = new SatelliteRepository();
+
+            float Distance = 0;
+            string[] Message = null;
+
+            dynamic reader = JsonConvert.DeserializeObject(Convert.ToString(JSON));
+            Distance = reader.distance != null ? reader.distance : 0;
+            Message = reader.message != null ? reader.message.ToObject<string[]>() : null;
+
+            if (Distance > 0 && Message != null && Message.Length > 0)
+            {
+                Satellite sat = repo.getSatellite(satellite_name.ToUpper());
+                sat.Distance = Distance;
+                sat.Message = Message;
+                return repo.updateSatellite(sat);
+            }
+            else return false;
+        }
+        #endregion
+
+        #region Private Functions
+        /// <summary>
+        /// Calcula la Posición del Emisor.
+        /// </summary>
+        /// <param name="DistanceKenobi">Distancia del Satélite Kenobi al Emisor.</param>
+        /// <param name="DistanceSkywalker">Distancia del Satélite Skywalker al Emisor.</param>
+        /// <param name="DistanceSato">Distancia del Satélite Sato al Emisor.</param>
+        /// <returns>Objeto Coordenada con la posición X e Y del Emisor.</returns>
+        private Coordinate getLocation(float DistanceKenobi, float DistanceSkywalker, float DistanceSato)
+        {
+            SatelliteRepository repo = new SatelliteRepository();
+            List<Satellite> SatelliteList = repo.getAllSatellites();
+
+            if (SatelliteList.Count != 3)
+                return null;
+
+            List<Point> PointList = new List<Point>();
+
+            Dictionary<string, float> Dict = new Dictionary<string, float>() { { "KENOBI", DistanceKenobi }, { "SKYWALKER", DistanceSkywalker }, { "SATO", DistanceSato } };
+            foreach(Satellite s in SatelliteList)
+                PointList.Add(new Point(s.Location.X, s.Location.Y, Dict[s.Name.ToUpper()]));
+
+            if (PointList.Count != 3)
+                return null;
+
+            double[] Location = Trilateration.Compute(PointList[0], PointList[1], PointList[2]);
+
+            Coordinate coordReturn = new Coordinate();
+            if (Location.Length == 2)
+            {
+                coordReturn.X = float.Parse(Location[0].ToString());
+                coordReturn.Y = float.Parse(Location[1].ToString());
+            }
+            else
+                coordReturn = null;
+
+            return coordReturn;
+        }
+        /// <summary>
+        /// Calcula la Posición del Emisor.
+        /// </summary>
+        /// <param name="Kenobi">Objeto Satellite Kenobi.</param>
+        /// <param name="Skywalker">Objeto Satellite Skywalker.</param>
+        /// <param name="Sato">Objeto Satellite Sato.</param>
+        /// <returns>Objeto Coordenada con la posición X e Y del Emisor.</returns>
+        private Coordinate getLocation(Satellite Kenobi, Satellite Skywalker, Satellite Sato)
+        {
+            if (Kenobi == null || Skywalker == null || Sato == null)
+                return null;
+
+            List<Point> PointList = new List<Point>();
+            List<Satellite> SatelliteList = new List<Satellite>() { Kenobi, Skywalker, Sato };
+            foreach (Satellite s in SatelliteList)
+                PointList.Add(new Point(s.Location.X, s.Location.Y, Convert.ToDouble(s.Distance)));
+
+            double[] Location = Trilateration.Compute(PointList[0], PointList[1], PointList[2]);
+
+            Coordinate coordReturn = new Coordinate();
+            if (Location != null && Location.Length >= 2)
+            {
+                coordReturn.X = float.Parse(Math.Round(Location[0],2).ToString());
+                coordReturn.Y = float.Parse(Math.Round(Location[1],2).ToString());
+            }
+            else
+                coordReturn = null;
+
+            return coordReturn;
+        }
+        /// <summary>
+        /// Descifra el mensaje del Emisor
+        /// </summary>
+        /// <param name="MessageKenobi">Mensaje recibido en el satélite Kenobi.</param>
+        /// <param name="MessageSkywalker">Mensaje recibido en el satélite Skywalker.</param>
+        /// <param name="MessageSato">Mensaje recibido en el satélite Sato.</param>
+        /// <returns>Mensaje Descifrado. Si no es posible descifrar, retorna null.</returns>
+        private string getMessage(string[] MessageKenobi, string[] MessageSkywalker, string[] MessageSato)
+        {
+            if (MessageKenobi == null || MessageSkywalker == null || MessageSato == null)
+                return null;
+
+            int MaxWords = 0;
+            MaxWords = MessageKenobi.Length > MessageSkywalker.Length ? MessageKenobi.Length : MessageSkywalker.Length;
+            MaxWords = MaxWords > MessageSato.Length ? MaxWords : MessageSato.Length;
+
+            string[] retMessage = new string[MaxWords];
+
+            for (int i = 0; i < MessageKenobi.Length; i++)
+                retMessage[i] = MessageKenobi[i].Trim() != "" ? MessageKenobi[i] : retMessage[i];
+
+            for (int i = 0; i < MessageSkywalker.Length; i++)
+                retMessage[i] = MessageSkywalker[i].Trim() != "" ? MessageSkywalker[i] : retMessage[i];
+
+            for (int i = 0; i < MessageSato.Length; i++)
+                retMessage[i] = MessageSato[i].Trim() != "" ? MessageSato[i] : retMessage[i];
+
+            bool messageDecrypted = true;
+            for (int i = 0; i < retMessage.Length && messageDecrypted; i++)
+                if (retMessage[i] == null || retMessage[i].Trim() == "")
+                    messageDecrypted = false;
+
+            return messageDecrypted? String.Join(" ", retMessage) : null;
+        }
+        /// <summary>
+        /// Deserializa un JSON en objetos Satellite
+        /// </summary>
+        /// <param name="JSON">JSON con los datos a deserializar.</param>
+        /// <param name="SplitResponse">Parámetro que indica si es para el controlador TopSecret o TopSecret_Split</param>
+        /// <returns>Lista de Satélites que posee el JSON.</returns>
+        private List<Satellite> deserializeTopSecret(dynamic JSON, bool SplitResponse = false)
+        {
+            JObject jsonParse = JObject.Parse(Convert.ToString(JSON));
+            List<JToken> jsonResults = null;
+            if (!SplitResponse)
+                jsonResults = jsonParse["satellites"].Children().ToList();
+            else
+                jsonResults = jsonParse.Children().ToList();
+
+            List<Satellite> SatelliteList = new List<Satellite>();
+
+            foreach (JToken r in jsonResults)
+            {
+                Satellite s = JsonConvert.DeserializeObject<Satellite>(r.ToString());
+                SatelliteList.Add(s);
+            }
+
+            return SatelliteList.Count() > 0 ? SatelliteList : null;
+        }
+        /// <summary>
+        /// Valida los Datos de un satélite
+        /// </summary>
+        /// <param name="sat">Objeto Satellite a validar.</param>
+        /// <returns>True en caso correcto. False en caso contrario.</returns>
+        private bool validateSatellite(Satellite sat)
+        {
+            return (sat.Distance != null && sat.Distance > 0) && (sat.Message != null && sat.Message.Length > 0) && (sat.Name != null && sat.Name != "") ? true : false;
+        }
+        /// <summary>
+        /// Valida los Datos de una Lista de Satélites
+        /// </summary>
+        /// <param name="list">Lista de Objetos Satellite.</param>
+        /// <returns>True en caso correcto. False en caso contrario.</returns>
+        private bool validateSatelliteList(List<Satellite> list)
+        {
+            bool Flag = true;
+            foreach (Satellite s in list)
+                if (Flag) Flag = validateSatellite(s);
+            return Flag;
+        }
+        #endregion
+    }
+}
